ffc0ba2bd18f05fadf6c9ee0c4c07b8228397bcd
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-26 19:41:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -249,6 +249,7 @@ def winning_team
 end
 
 def player_with_longest_name
+  current_longest = 0
   current_player_name = nil
   
   game_hash.each do |place, team|
@@ -257,7 +258,6 @@ def player_with_longest_name
         data.each do |player|
           if player[:player_name].length > current_longest
             current_player_name = player[:player_name]
-            current_longest = player[:player_name]
           end
         end  
       end
